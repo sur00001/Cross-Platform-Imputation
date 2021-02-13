1beeb79fa9e83e030b854d0ea94b74412ef7ad55
sur00001 <47928858+sur00001@users.noreply.github.com>
2021-02-13 15:07:15 -0600

    Add files via upload
    New glmres function with the 3 parameters!

--- GetGLMResults.R
+++ GetGLMResults.R
@@ -10,14 +10,23 @@ alphas = function(res.list){alphas=map_dbl(res.list, "al", .default = NA)}
 lamdas= function(res.list){map_dbl(res.list, "lam", .default = NA)}
 
 #glm resuts
-glm.res = function(res.list){
+glm.res = function(res.list,r=FALSE,r.sites){ #r=FALSE whole chromosome
   preds=lapply(res.list, "[[" , "y_pred" )
   y_pred = matrix(NA,nrow=nrow(y_test),ncol=ncol(y_test))
+  if (r==TRUE){ #random sites are used 
+    j=1
+    for (i in r.sites){
+      y_pred[,i]=preds[[j]]
+      j=j+1}
+    glm.res = summarize_res(y_pred[,r.sites],y_test[,r.sites])
+  } else{
   j=1
   for (i in 1:ncol(y_test)){
     y_pred[,i]=preds[[j]]
     j=j+1}
   glm.res = summarize_res(y_pred,y_test)
+  }
+
   return(glm.res)
 }
 
